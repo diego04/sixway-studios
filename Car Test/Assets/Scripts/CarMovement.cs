c1ba062cd32b3dde31a20f1b43f6f6becf9b12ea
Christopher Li Sheung Ying <lisheung@ualberta.ca>
2016-04-24 18:41:17 -0600

    Unity Car Files
    Unity test for a car. It drives around. Cannot figure out how to make
    create/make wheel models turn.

---
+++ Car Test/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarMovement : MonoBehaviour {
+
+	public float MotorForce;
+	public float SteerAngle;
+    public float BrakeForce;
+
+	public WheelCollider FLColWheel;
+	public WheelCollider FRColWheel;
+	public WheelCollider BLColWheel;
+	public WheelCollider BRColWheel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float accel = Input.GetAxis ("Vertical") * MotorForce;
+		float steer = Input.GetAxis ("Horizontal") * SteerAngle;
+
+		BLColWheel.motorTorque = accel;
+		BRColWheel.motorTorque = accel;
+        FLColWheel.motorTorque = accel;
+        FRColWheel.motorTorque = accel;
+
+		FLColWheel.steerAngle = steer;
+		FRColWheel.steerAngle = steer;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            BLColWheel.brakeTorque = BrakeForce;
+            BRColWheel.brakeTorque = BrakeForce;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            BLColWheel.brakeTorque = 0;
+            BRColWheel.brakeTorque = 0;
+        }
+
+    }
+}
